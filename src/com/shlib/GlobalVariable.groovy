f2fab8f3446ae89d6e9a6c9589e9ab2dd468d185
Chaitanya Senapati <csenapati12@gmail.com>
2019-04-18 21:28:55 +0530

    Update GlobalVariable.groovy

--- src/com/shlib/GlobalVariable.groovy
+++ src/com/shlib/GlobalVariable.groovy
@@ -3,5 +3,14 @@ package com.shlib
 class GlobalVariable
  {
    static String gbvar = "MyVar"
+  
+   def info(message) {
+    echo "INFO: ${message}"
+   }
+
+  def warning(message) {
+    echo "WARNING: ${message}"
+  }
+ 
 }
 
